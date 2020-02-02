6c784e9de8e2b8f3aa1035d975fe058b6e6d7e35
Brigette LeVert <bnl.levert@gmail.com>
2020-02-02 19:32:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,27 +2,27 @@
 
 # binding.pry 
 
-class Artist
-  attr_accessor :name
+# class Artist
+#   attr_accessor :name
 
-  @@songs = []
+#   @@songs = []
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end 
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end 
 
 
- def add_song(song)
-    self.songs << song
-    song.artist = self
- end
+# def add_song(song)
+#     self.songs << song
+#     song.artist = self
+# end
   
-  def songs
-    @songs.select {|song| song.artist}
-  end
+#   def songs
+#     @songs.select {|song| song.artist}
+#   end
 
   
-end
+# end
 
 
